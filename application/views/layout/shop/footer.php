9b6a47df4eaec82720bf92d716394b295bd63b4c
syahnur197 <syahnurnizam197@gmail.com>
2017-10-22 23:50:15 +0800

    css fixed
    fix margin top for footer

--- application/views/layout/shop/footer.php
+++ application/views/layout/shop/footer.php
@@ -1,6 +1,6 @@
 
 		<!-- Footer -->
-		<footer class="py-5 bg-dark">
+		<footer class="py-5 mt-5 bg-dark">
 			<div class="container">
 				<p class="m-0 text-center text-white">Copyright &copy; eShop Sdn. Bhd. 2017</p>
 			</div>
